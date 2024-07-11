78f629d099463b47bf8fdd6f7e8485f67300ec77
rupeshtr78 <rupeshtr78@gmail.com>
2024-07-11 16:09:21 -0700

    minio aws

--- main.go
+++ main.go
@@ -10,35 +10,43 @@ import (
 )
 
 func main() {
-	ListBuckets()
+	client, err := GetS3Client("minio")
+	if err != nil {
+		panic(err)
+	}
+	buckets, err := ListBuckets(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(buckets)
 }
 
-func ListBuckets() {
+func ListBuckets(client *s3.S3) (*s3.ListBucketsOutput, error) {
+
+	// Get the list of buckets
+	result, err := client.ListBuckets(nil)
+	if err != nil {
+		return nil, fmt.Errorf("error listing buckets: %v", err)
+	}
+
+	return result, nil
+}
+
+func GetS3Client(profileName string) (*s3.S3, error) {
 	// Create a new session for minio
 	// use profile credentials using endpoint url
 	sess, err := session.NewSessionWithOptions(session.Options{
-		Profile: "minio",
+		Profile: profileName,
 		Config: aws.Config{
 			Region:           aws.String("us-west-1"),
 			Endpoint:         aws.String("http://10.0.0.213:9000"),
 			S3ForcePathStyle: aws.Bool(true),
-			Credentials:      credentials.NewSharedCredentials("", "minio"),
+			Credentials:      credentials.NewSharedCredentials("", profileName),
 		},
 	})
 	if err != nil {
-		fmt.Printf("Error creating session: %v", err)
-		return
+		return nil, fmt.Errorf("error creating session: %v", err)
 	}
 
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	// Get the list of buckets
-	result, err := svc.ListBuckets(nil)
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	fmt.Println("Success", result)
+	return s3.New(sess), nil
 }
